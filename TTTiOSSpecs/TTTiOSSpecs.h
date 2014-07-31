62b175cae1b06a770aecf66f7dfc0a9c6e03a9a3
alexreedhill <ahill1994@student.elgin.edu>
2014-07-31 09:58:24 -0500

    Initial Commit

---
+++ TTTiOSSpecs/TTTiOSSpecs.h
@@ -0,0 +1,19 @@
+//
+//  TTTiOSSpecs.h
+//  TTTiOSSpecs
+//
+//  Created by Alex Hill on 7/31/14.
+//  Copyright (c) 2014 Alex Hill. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for TTTiOSSpecs.
+FOUNDATION_EXPORT double TTTiOSSpecsVersionNumber;
+
+//! Project version string for TTTiOSSpecs.
+FOUNDATION_EXPORT const unsigned char TTTiOSSpecsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TTTiOSSpecs/PublicHeader.h>
+
+
